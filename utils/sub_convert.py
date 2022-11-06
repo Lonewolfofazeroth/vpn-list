3684dcded6ecc2d3d82c107d93eb1117e21e4199
songtao1873 <479081675@qq.com>
2022-11-06 16:27:40 +0800

    update convert

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -528,7 +528,10 @@ class sub_convert():
                             print(f'SSR format error, content:{remarks_part}')
                     yaml_url.setdefault('name', '"' + urllib.parse.unquote(remarks) + '"')
                     server_part_list = re.split(':', part_list[0])
-                    yaml_url.setdefault('server', server_part_list[0])
+                    if "NULL" in server_part_list[0]:
+                        continue
+                    else:
+                        yaml_url.setdefault('server', server_part_list[0])
                     yaml_url.setdefault('port', server_part_list[1])
                     yaml_url.setdefault('type', 'ssr')
                     ssr_cipher = ["aes-128-gcm", "aes-192-gcm", "aes-256-gcm", "aes-128-cfb", "aes-192-cfb", "aes-256-cfb", "aes-128-ctr", "aes-192-ctr", "aes-256-ctr", "rc4-md5", "chacha20-ietf", "xchacha20", "chacha20-ietf-poly1305", "xchacha20-ietf-poly1305"]
