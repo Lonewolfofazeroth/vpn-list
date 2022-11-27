0bc82ce744757ea2da424fd3f05890e9bc4692c8
songtao1873 <479081675@qq.com>
2022-11-27 16:15:36 +0800

    update convert

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -445,10 +445,11 @@ class sub_convert():
                     config_first_decode_list = sub_convert.base64_decode(ss_content_array[0]).split(':')
                     # include server port
                     config_second_list = ss_content_array[1].split(':')
-                    if "::" in config_second_list[:-1]:
+                    server_address = ':'.join(config_second_list[:-1]).replace("]","").replace("[","")
+                    if "::" in server_address:
                         continue
                     else:
-                        yaml_url.setdefault('server', (':'.join(config_second_list[:-1])).replace("]","").replace("[",""))
+                        yaml_url.setdefault('server', server_address)
                     yaml_url.setdefault('port', config_second_list[-1])
                     yaml_url.setdefault('type', 'ss')
                     ss_cipher = ["aes-128-gcm", "aes-192-gcm", "aes-256-gcm", "aes-128-cfb", "aes-192-cfb", "aes-256-cfb", "aes-128-ctr", "aes-192-ctr", "aes-256-ctr", "rc4-md5", "chacha20-ietf", "xchacha20", "chacha20-ietf-poly1305", "xchacha20-ietf-poly1305"]
