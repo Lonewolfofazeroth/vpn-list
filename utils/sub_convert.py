39bf26162440e6ace593b61fe0cec9aaaf970ea6
songtao1873 <479081675@qq.com>
2023-02-15 20:30:18 +0800

    test

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -559,7 +559,7 @@ class sub_convert():
                             obfs_param = sub_convert.base64_decode(
                                 item.replace('obfsparam=', ''))
                             if obfs_param != '':
-                                yaml_url.setdefault('obfs-param', '"' + obfs_param + '"')
+                                yaml_url.setdefault('obfs-param', "'" + obfs_param + "'")
                             else:
                                 yaml_url.setdefault('obfs-param', '""')
                         elif 'protoparam=' in item:
